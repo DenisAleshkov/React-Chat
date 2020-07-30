f884888261d6ba8688b6ddc2d9369ce86d95ca3b
DenisAleshkov <aleshkov592@gmail.com>
2020-07-30 20:03:20 +0300

    write css for chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -27,34 +27,38 @@ const Chat = ({users, messages, userName, roomId, onAddMessage}) => {
     return (
         <div className="chat-wrapper">
             <div className="chat">
-                <div className="chat-users users">
-                    <h1>Room:{ roomId }</h1>
-                    <div className="users-title">Online({ users.length })</div>
-                    <div className="users-view user">
-                        {users.map( (name, index) => <div key={ index } className="user-view">{ name }</div> )}
-                    </div>
-                </div>
-                <div className="chat-messages messages">
-                    <div className="messages-box" ref={messagesRef}>
-                        {messages.map( (message) => (
-                            <div className="messages-view">
-                                <p className="messages-text">{ message.text }</p>
-                                <span className="messages-name">{ message.userName }</span>
+                <div className="chat-box">
+                    <h1>Room: {roomId}</h1>
+                        <div className="chat-content">
+                            <div className="chat-users users">
+                                <div className="users-title">Online({ users.length })</div>
+                                <div className="users-view user">
+                                     {users.map( (name, index) => <div key={ index } className="user-view">{ name }</div> )}
+                                </div>
                             </div>
-                           )
-                        )
-                        }
-                    </div>
-                    <div className="messages-inputs">
-                        <textarea
-                            className="messages-textare"
-                            value={messagesValue}
-                            onChange = { (e) => setMessagesValue(e.target.value) }
-                        ></textarea>
-                        <button  
-                            className="messages-button"
-                            onClick={ onSendMessage }
-                            >Send</button>
+                            <div className="chat-messages messages">
+                                <div className="messages-box" ref={messagesRef}>
+                                    {messages.map( (message) => (
+                                        <div className="messages-view">
+                                            <p className="messages-text">{ message.text }</p>
+                                            <span className="messages-name">{ message.userName }</span>
+                                        </div>
+                                       )
+                                    )
+                                    }
+                                </div>
+                                <div className="messages-inputs">
+                                    <textarea
+                                        className="messages-textarea"
+                                        value={messagesValue}
+                                        onChange = { (e) => setMessagesValue(e.target.value) }
+                                    ></textarea>
+                                    <button  
+                                        className="messages-button"
+                                        onClick={ onSendMessage }
+                                        >Send</button>
+                                </div>
+                        </div>
                     </div>
                 </div>
             </div>
